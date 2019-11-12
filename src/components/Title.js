bbc95287ac5286459eeab544c41b7ad78808e618
Venkat <v-vsrinivasa@expedia.com>
2019-11-12 23:03:07 +0530

    first commit

---
+++ src/components/Title.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export default function Title({name,title}) {
+    return (
+        <div className="row">
+          <div className=".col-10 max-auto my-2 text-center text-title">  
+          <h1 className="tesxt-capitalize font-weight-bold">
+              {name} <strong className="text-blue">
+                {title}
+              </strong>
+              </h1>
+          </div>
+        </div>
+    )
+}
