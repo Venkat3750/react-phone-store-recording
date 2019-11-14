e8f54a05512e3314724a4d1d9b7efa4b12d2a002
Venkat <v-vsrinivasa@expedia.com>
2019-11-14 23:16:30 +0530

    second commit

--- src/components/Cart/CartTotals.js
+++ src/components/Cart/CartTotals.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
-
-export default function CartTotals({value}) {
+import PayPalButton from './PayPalButton';
+export default function CartTotals({value,history}) {
     const{cartSubTotal,cartTax,cartTotal,clearCart} = value;
     return (
        <React.Fragment>
@@ -17,18 +17,22 @@ export default function CartTotals({value}) {
                            <h5>
                                <span className="text-title">
                                    subtotal : </span>   
-                                   <strong>${cartSubTotal}</strong>
+                                   <strong>Rs.{cartSubTotal}</strong>
                                    </h5>
                                    <h5>
                                <span className="text-title">
                                    tax : </span>   
-                                   <strong>${cartTax}</strong>
+                                   <strong>Rs.{cartTax}</strong>
                                    </h5>
                                    <h5>
                                <span className="text-title">
                                    total : </span>   
-                                   <strong>${cartTotal}</strong>
+                                   <strong>Rs.{cartTotal}</strong>
                                    </h5>
+                                   <PayPalButton total = {cartTotal} clearCart={clearCart}
+                                   history={history}/>
+                                       
+                                  
                        </div>
                </div>
            </div>
